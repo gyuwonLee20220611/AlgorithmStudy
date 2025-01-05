24cbc36f6a4d0b27b5b0e9a174cafbf649db8215
gyuwonLee20220611 <guwon2784@kookmin.ac.kr>
2025-01-05 17:59:00 +0900

    2025/01/05

---
+++ Graph_Theory/Disjoint_Sets/0.Example.py
@@ -0,0 +1,33 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parents(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+
+v, e = map(int, input().split())
+
+parents = [i for i in range(v + 1)]
+
+for i in range(e):
+    a, b = map(int, input().split())
+    union_parents(parents, a, b)
+
+print("각 원소가 속한 그룹: ", end = '')
+for i in range(1, v + 1):
+    print(find_parent(parents, parents[i]), end = ' ')
+
+print()
+
+print("부모 테이블: ", end = '')
+for i in range(1, v + 1):
+    print(parents[i], end = ' ')
