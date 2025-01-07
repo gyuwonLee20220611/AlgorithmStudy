9dc105159a4bc33600ca2ca1e9017509c9ec45ca
gyuwonLee20220611 <guwon2784@kookmin.ac.kr>
2025-01-07 21:53:29 +0900

    2025/01/07

--- Graph_Theory/BFS/7.7576_G5.py
+++ Graph_Theory/BFS/7.7576_G5.py
@@ -1,9 +1,9 @@
 from collections import deque
 import sys
+
 input = sys.stdin.readline
 
-def bfs(graph, x, y, n, m):
-    que = deque([(x, y)])
+def bfs(n, m):
     move_x = [-1, 0, 1, 0]
     move_y = [0, -1, 0, 1]
     while que:
@@ -14,6 +14,7 @@ def bfs(graph, x, y, n, m):
             dy = y + move_y[i]
             if not 0 <= dx < n or not 0 <= dy < m or graph[dx][dy] == -1 or graph[dx][dy] == 1:
                 continue
+
             else:
                 if graph[dx][dy] != 0 and graph[x][y] + 1 < graph[dx][dy]:
                     graph[dx][dy] = graph[x][y] + 1
@@ -25,13 +26,18 @@ def bfs(graph, x, y, n, m):
 
 m, n = map(int, input().split())
 graph = []
+que = deque([])
 for i in range(n):
-    graph.append(list(map(int, input().split())))
+    a = list(map(int, input().split()))
+    graph.append(a)
 
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 1:
-            bfs(graph, i, j, n, m)
+            que.append((i, j))
+
+bfs(n, m)
+
 max_num = 0
 
 for i in graph:
@@ -42,5 +48,3 @@ for i in graph:
         max_num = max(max_num, max(i))
 else:
     print(max_num - 1)
-
-
